cf3007a53388ce5e8e50da6aa8b39138b8071477
DestinyJoyner <destinyjoyner@pursuit.org>
2024-02-16 13:56:44 -0500

    validation on videogame id

--- controllers/videogamesController.js
+++ controllers/videogamesController.js
@@ -8,4 +8,39 @@ videogames.get("/", (req,res) => {
 })
 
 
+videogames.get("/:videogameID", (req, res) => {
+    const videogameID = req.params.videogameID
+
+    console.log(Number(videogameID))
+    if(Number(videogameID)){
+        res.status(200).json({message: videogameID})
+    }
+    else {
+        res.status(404).json({
+            error : "id must be numeric value"
+        })
+    }
+
+    
+})
+
+
+
+
 module.exports = videogames
+
+
+// http://localhost:3001/videogame/8
+/* req = {
+    body: {
+
+    },
+    params : {
+        videogameID: "destiny"
+    },
+    query : {
+        name : "code"
+    }
+
+} */
+
