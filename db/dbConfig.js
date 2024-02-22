0af933029e0238db0875ae08f8a6d6c6455fb21d
DestinyJoyner <destinyjoyner@pursuit.org>
2024-02-22 11:48:37 -0500

    added password connection key

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -7,7 +7,8 @@ const cn = {
     host: process.env.PG_HOST,
     port:process.env.PG_PORT,
     user:process.env.PG_USER,
-    database:process.env.PG_DATABASE
+    database:process.env.PG_DATABASE,
+    password:process.env.PG_PASSWORD
 }
 
 const db = pgp(cn)
